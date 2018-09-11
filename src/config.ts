fac2a4a5ef73a99d64023173991d4c0804ce2831
Aaron RJ <aresinodeveloper@gmail.com>
2018-09-11 17:45:00 +0200

    added firebase library

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCi7inrSyxmxc1BYLD0Pj8my0XBAnOYgiM",
+    authDomain: "expensewenger.firebaseapp.com",
+    databaseURL: "https://expensewenger.firebaseio.com",
+    projectId: "expensewenger",
+    storageBucket: "expensewenger.appspot.com",
+    messagingSenderId: "98488418857"
+};
